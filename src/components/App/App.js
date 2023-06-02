f7264589de9a035c30b5fd9fcbe733b52dda895b
yeXela13 <alexey13.90@mail.ru>
2023-06-02 09:27:59 +0300

    Загрузить CRA, структуру проекта

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import '../index.css';
+import React, { useCallback, useState, useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+function App() {
+
+
+
+return (
+<></>
+);
+
+}
+
+export default App;
